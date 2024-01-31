9d882309eea19d828659b252ee8b4bd98a968498
Dubrovskiy-Konstantin <kd.5556600@gmail.com>
2024-01-31 23:19:45 +0300

    Task 1.2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(@"C:\work\testWPF\ConsoleApp1\Files");
+                foreach (FileInfo file in directory.EnumerateFiles())
+                    file.Delete();
+
+                //Задание 1.1
+                Generator generator = new Generator();
+                generator.GenereFiles(directory);
+                //Задание 1.2
+                Combinator combinator = new Combinator();
+                int deletedLines;
+                string deletePattern = "";
+                combinator.Combine(directory, deletePattern, out deletedLines);
+                Console.WriteLine(deletedLines);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
